1b2937eacacd5bf49e0a0b1a4fede91eb9e220d4
Henri <henridvf@gmail.com>
2021-09-29 13:35:06 +0100

    Added unit tests; refactored collection display function for better unit testing

--- functions.php
+++ functions.php
@@ -25,7 +25,7 @@ function getAllWatches(): array
 function getImagePath(string $imagePath): string
 {
     $path='';
-    $imageStartsWith = substr($imagePath, strlen('http'));
+    $imageStartsWith = substr($imagePath, 0, strlen('http'));
     if (!$imageStartsWith) {
         $path = 'https://via.placeholder.com/250/FAFAD2';
     } elseif ($imageStartsWith === 'http') {
@@ -34,4 +34,45 @@ function getImagePath(string $imagePath): string
         $path = 'images/' . $imagePath;
     }
     return $path;
+}
+
+/**
+ * Constructs the entire collection into displayable format
+ * @return string
+ */
+function displayCollection(): string
+{
+    $display = '';
+    $watches = getAllWatches();
+
+    foreach ($watches as $watch) {
+        // create and add to display collection
+        $display .= createCollectionItem($watch);
+    };
+    return $display;
+}
+
+/**
+ * Creates the display markup for a single collection item
+ * @param $watch
+ * @return string
+ */
+function createCollectionItem($watch): string
+{
+    $coll_item = '<div class="collection_item">';
+    $img_file = is_null($watch["image"]) ? '' : $watch["image"];
+    $img = getImagePath($img_file);
+    $coll_item .= '<img src="' . $img . '" alt="Picture of a ' . $watch['name'] . '" />';
+    $coll_item .= '<h5>' . $watch["brand_name"] . '</h5>';
+    $coll_item .= '<p>' . $watch["name"] . '</p>';
+    $coll_item .= '<hr>';
+    $coll_item .= '<div class="split"><span><strong>Price</strong></span><span>£ ' . $watch["price"] .
+        '</span></div>';
+    $coll_item .= '<div class="split"><span><strong>Purchased</strong></span><span>' .
+        $watch["purchase_date"] . '</span></div>';
+    $coll_item .= '<hr>';
+    $coll_item .= '<p>' . $watch["notes"] . '</p>';
+    $coll_item .= '</div>';
+
+    return $coll_item;
 }
